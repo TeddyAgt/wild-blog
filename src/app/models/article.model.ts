a6b0af81817272310cecfb00ba3d774e0aed8c26
Teddy Aboim <teddy.aboim@gmail.com>
2025-02-25 15:23:03 +0100

    add conditionnal rendering for article list

--- src/app/models/article.model.ts
+++ src/app/models/article.model.ts
@@ -1,9 +1,11 @@
 export default interface Article {
+    id: number;
     title: string;
     content: string;
     image: string;
     createdAt: Date;
     isPublished: boolean;
+    isLiked: boolean;
     likeCount: number;
     categoryName: string;
 }
