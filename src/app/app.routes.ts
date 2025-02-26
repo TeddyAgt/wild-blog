6e7c09c886e707c38d77974c60fcb35fb6b730d1
Teddy Aboim <teddy.aboim@gmail.com>
2025-02-26 14:25:07 +0100

    add contact form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from "@angular/router";
 import { ArticlePageComponent } from "./pages/article-page/article-page.component";
+import { ContactPageComponent } from "./pages/contact-page/contact-page.component";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { NotFoundPageComponent } from "./pages/not-found-page/not-found-page.component";
 
 export const routes: Routes = [
     { path: "", component: HomePageComponent },
     { path: "article/:id", component: ArticlePageComponent },
+    { path: "contact", component: ContactPageComponent },
     { path: "**", component: NotFoundPageComponent },
 ];
